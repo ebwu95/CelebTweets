2c078b0719454128c4be015f89628f33d7f60bda
Evan Wu <evanwu@evans-air.lan>
2022-06-24 21:26:28 -0400

    added backend

---
+++ server.js
@@ -0,0 +1,40 @@
+const Twit = require('twit')
+const config = require('./config')
+const express = require('express')
+const app = express();
+const T = new Twit(config);
+const PORT = 8080;
+
+
+app.get('/:name', (req, res) => {
+    const { name } = req.params; 
+    query = 'from:' + name + ' -is:retweet -is:reply -is:quote -has:links -has:mentions -has:links -has:media -has:images -has:videos -RT'
+    
+    let params = {
+        q: query,  
+        lang: 'en',
+        count: 50,
+    }
+    function gotData(err, data, response){
+        if (err) {
+            res.status(400).send('no');
+        }
+        curTweets = [];
+        ids = []
+        data.statuses.forEach(tweet => {
+            curTweets.push(tweet.text);
+            ids.push(tweet.id);
+        });
+
+        var idx = Math.floor(Math.random() * curTweets.length);
+        res.status(200).send({
+            tweet: curTweets[idx],
+            id: ids[idx]
+        })
+    }
+    T.get('search/tweets', params, gotData);
+});
+
+app.listen(
+    PORT,
+)
